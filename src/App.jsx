4db0f1ceb74e202258ebb85232fee1f2fa2ba1dd
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-07-27 10:34:25 +0200

    add nav section component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      Welcome to AirBnb Experiences clone
+    <div>
+      <Navbar />
     </div>
   )
 }
