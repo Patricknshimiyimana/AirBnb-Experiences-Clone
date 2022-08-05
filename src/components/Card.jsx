4a88c4c367e15ff95a9f1f352b7305dc61e314b3
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-08-05 17:27:26 +0200

    sold out & online badge

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,28 @@
-import React from "react"
-import starImg from '../assets/star.png';
-import cardImage from '../assets/katie-zaferes.png'
+import React from "react";
+import starImg from "../assets/star.png";
+import cardImage from "../assets/katie-zaferes.png";
 
 export default function Card(props) {
-    return (
-        <div className="card">
-            <img src={cardImage} className="card--image" />
-            <div className="card--stats">
-                <img src={starImg} className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray"> ({props.reviewCount}) • Online</span>
-                <span className="gray">{props.location}</span>
-            </div>
-            <p className="card--title">{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
-        </div>
-    )
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+  return (
+    <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={cardImage} className="card--image" />
+      <div className="card--stats">
+        <img src={starImg} className="card--star" />
+        <span>{props.rating}</span>
+        <span className="gray"> ({props.reviewCount}) • Online</span>
+        <span className="gray">{props.location}</span>
+      </div>
+      <p className="card--title">{props.title}</p>
+      <p>
+        <span className="bold">From ${props.price}</span> / person
+      </p>
+    </div>
+  );
 }
