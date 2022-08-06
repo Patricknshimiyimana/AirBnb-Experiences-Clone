6755534f663562ffdd949bb3f2438f0017b3f6b1
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-08-06 18:17:59 +0200

    update image path import

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,4 +1,6 @@
-import imgs from '../../src/assets/wedding-photography.png'
+import weddingPic from './wedding-photography.png';
+import katieZaferes from './katie-zaferes.png';
+import bikePic from './mountain-bike.png';
 
 export default [
     {
@@ -6,7 +8,7 @@ export default [
         title: "Life Lessons with Katie Zaferes",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        coverImg: "../../src/assets/katie-zaferes.png",
+        coverImg: katieZaferes,
         stats: {
             rating: 5.0,
             reviewCount: 6
@@ -19,7 +21,7 @@ export default [
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
-        coverImg: "../../src/assets/wedding-photography.png",
+        coverImg: weddingPic,
         stats: {
             rating: 5.0,
             reviewCount: 30
@@ -32,7 +34,7 @@ export default [
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "../../src/assets/mountain-bike.png",
+        coverImg: bikePic,
         stats: {
             rating: 4.8,
             reviewCount: 2
